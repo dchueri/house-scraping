b14a203efdfaa255f1b1db983889f7e2997cf8c6
diego <diegochueri@gmail.com>
2023-02-24 11:48:39 -0400

    feat: add puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("ok");
+import puppeteer from "puppeteer";
+
+console.log("Starting Bot");
+
+const link =
+  "https://sc.olx.com.br/florianopolis-e-regiao/imoveis/alugo-casa-no-canto-da-lagoa-1150299446";
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(link, { waitUntil: "domcontentloaded" });
+  await page.evaluate(() => window.stop());
+  await page.screenshot({ path: "house.png" });
+
+  await browser.close();
+})();
